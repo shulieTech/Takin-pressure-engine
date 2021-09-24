fd7a3b1ea0585270ee1b276063ec3471f1e907e9
liangxiaorong <>
2021-09-24 19:16:39 +0800

    修改构建脚本

--- buildImage.sh
+++ buildImage.sh
@@ -26,17 +26,13 @@ IMAGE_TAG="latest"
 
 #自定义参数 （以下变量需要根据自己实际情况进行修改）
 #JMETER源码路径
-#JMETER_SOURCE_PATH=~/develop/workspaces/gitWorkspaces/Takin-jmeter
-JMETER_SOURCE_PATH=/Users/moriarty/work/shulie/Takin-jmeter
+JMETER_SOURCE_PATH=~/develop/workspaces/gitWorkspaces/Takin-jmeter
 #Gradle目录
-#GRADLE_HOME=~/.gradle/wrapper/dists/gradle-6.6-bin/dflktxzwamd4bv66q00iv4ga9/gradle-6.6
-GRADLE_HOME=/Users/moriarty/dev/gradle-6.6/
+GRADLE_HOME=~/.gradle/wrapper/dists/gradle-6.6-bin/dflktxzwamd4bv66q00iv4ga9/gradle-6.6
 #Maven settings文件路径
-#MAVEN_SETTINGS_PATH=~/develop/.m2/settings.xml
-MAVEN_SETTINGS_PATH=/Users/moriarty/dev/apache-maven-3.6.3/conf/settings.xml
+MAVEN_SETTINGS_PATH=~/develop/.m2/settings.xml
 #压测引擎项目源码根目录
-#PRESSURE_ENGINE_SOURCE_PATH=~/develop/workspaces/gitWorkspaces/Takin-pressure-engine
-PRESSURE_ENGINE_SOURCE_PATH=/Users/moriarty/work/shulie/Takin-pressure-engine
+PRESSURE_ENGINE_SOURCE_PATH=~/develop/workspaces/gitWorkspaces/Takin-pressure-engine
 
 log() {
     echo -e "\033[40;37m $1 \033[0m"
